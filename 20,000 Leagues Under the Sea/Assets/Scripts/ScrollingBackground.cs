2302ae9f4137690019a1f37363610d0201b631ca
Kenneth Liang <kenneth-liang@hotmail.com>
2021-02-14 00:59:51 -0800

    Game over scene complete

--- 20,000 Leagues Under the Sea/Assets/Scripts/ScrollingBackground.cs
+++ 20,000 Leagues Under the Sea/Assets/Scripts/ScrollingBackground.cs
@@ -76,81 +76,26 @@ public class ScrollingBackground : MonoBehaviour
         }
         lastScreenPosition = transform.position;
     }
-    /*public float backgroundSize;
 
-    private Transform cameraTransform;
-    private Transform[] layers;
-    private float viewZone = 10;
-    private int leftIndex;
-    private int rightIndex;
-    private float lastCameraX;
-
-    public float paralaxSpeed; 
-
-
-    private void Start()
-    {
-        cameraTransform = Camera.main.transform;
-        lastCameraX = cameraTransform.position.x;
-
-        layers = new Transform[transform.childCount];
-
-        for(int i=0; i< transform.childCount; i++)
-        {
-            layers[i] = transform.GetChild(i);
-        }
-
-        leftIndex = 0;
-        rightIndex = layers.Length - 1;
-
-    }
-
-    private void Update()
-    {
-
-        float deltaX = cameraTransform.position.x - lastCameraX;
-        transform.position += Vector3.right * (deltaX * paralaxSpeed);
-
-        lastCameraX = cameraTransform.position.x;
-        if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
-        {
-            ScrollLeft();
-        }
-        if (cameraTransform.position.x > (layers[rightIndex].transform.position.x- viewZone))
-        {
-            ScrollRight();
-        }
-    }
-
-
-
-    private void ScrollLeft()
-    {
-        int lastRight = rightIndex;
-        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
-        leftIndex = rightIndex;
-        rightIndex--;
-
-        if(rightIndex < 0)
-        {
-            rightIndex = layers.Length -  1;
-        }
-    }
-
-
-
-
-    private void ScrollRight()
-    {
-        int lastLeft = leftIndex;
-        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
-        rightIndex = leftIndex;
-        leftIndex++;
-
-        if (leftIndex == layers.Length)
-        {
-            leftIndex = 0;
-        }
-    }*/
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* resource code inspired from https://pressstart.vip/tutorials/2019/05/1/94/parallax-effect-in-unity.html */ 
