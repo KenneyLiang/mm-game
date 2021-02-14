cfe8ccc414a68f9615f361d91922b0e4a71dce6c
Kenneth Liang <kenneth-liang@hotmail.com>
2021-02-14 03:29:29 -0800

    added background music for menu

---
+++ 20,000 Leagues Under the Sea/Assets/Scripts/GameOverMusic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMusic : MonoBehaviour
+{
+    public void SetUp()
+    {
+        gameObject.SetActive(true);
+    }
+}
